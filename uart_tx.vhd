284ece778d96faac0c1b0ec177e0032fcece7e74
Berk M. Kuzu <156541103+AzYbb66@users.noreply.github.com>
2024-12-02 20:24:51 +0300

    Update uart_tx.vhd

--- uart_tx.vhd
+++ uart_tx.vhd
@@ -10,18 +10,13 @@ STOP_BIT : integer := 1
 port (
 Tx_Done_Tick_O : out std_logic;
 Tx_O : out std_logic;
-Data_out : out std_logic_vector(7 downto 0);
-Rx_Done_Tick_O : out std_logic;
 Clk : in std_logic;
 Tx_Start_Tick_I : in std_logic := '0';
 D_in : in std_logic_vector(7 downto 0);
-Rx_I : in std_logic);
 end uart;
 architecture Behavioral of uart is
 type T_State is (Tx_IDLE,Tx_START,Tx_DATA,Tx_STOP);
 signal Tx_State :T_State:=Tx_IDLE;
-type R_State is (Rx_IDLE,Rx_START,Rx_DATA,Rx_STOP);
-signal Rx_State :R_State:=Rx_IDLE;
 constant bit_timer_lim :integer := CLK_FREQ/BAUD_RATE;
 constant halfbit_timer_lim :integer := bit_timer_lim/2;
 signal bit_timer_Tx :integer := 0;
@@ -41,14 +36,12 @@ case Tx_State is
 when Tx_IDLE =>
 Tx_O <= '1';
 Tx_Done_Tick_O <= '0';
---tx_buffer <= x"00";
 if(Tx_Start_Tick_I = '1') then
 tx_buffer <=D_in;
 Tx_O <= '0';
 Tx_State <= Tx_START;
 end if;
 when Tx_START =>
---Tx_O <= '0';
 if bit_timer_Tx = bit_timer_lim-1 then
 bit_timer_Tx <= 0;
 Tx_State <= Tx_DATA;
